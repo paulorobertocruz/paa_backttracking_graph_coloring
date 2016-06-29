59febb8872b3dc02532488348cf34ebcf79f94b9
paulo <paulo.cruz9@gmail.com>
2016-06-29 02:14:18 -0300

    melhorando o plotTable.py para receber argumentos e gerar resultado automatisado

--- PlotTable.py
+++ PlotTable.py
@@ -1,32 +1,42 @@
 import matplotlib.pyplot as plt
 import networkx as nx
-import grapth_star
+import sys
 from backtrack import Grafo
+
+if len(sys.argv) < 2:
+    exit("ta faltando coisa")
+
+filename = sys.argv[1]
+
+grafos = __import__("grafos."+filename, globals(), locals(), [filename, filename], 0)
+
+mapa = Grafo()
+mapa.setNodes(grafos.nodes)
+mapa.setVertex(grafos.vertex)
+
+nodes = grafos.nodes
+vertex = grafos.vertex
+
+if mapa.colora_node(0):
+    print("tudo ok")
+else:
+    print("não deu solução")
 
 G = nx.Graph()
 
-nodes = ["a","b","c","d"]
-vertex = [[1,1,0,1],[1,1,1,1],[0,1,1,1],[1,1,1,1]]
-
-mapa = Grafo()
-mapa.setNodes(nodes)
-mapa.setVertex(vertex)
-mapa.colora_node(0)
-
-
-G.add_nodes_from(nodes, color = "y")
+G.add_nodes_from(grafos.nodes, color = "y")
 pos = nx.random_layout(G)
 color = nx.get_node_attributes(G,'color')
 
 
-for i in range(0,len(vertex)):
-	for j in range(0,len(vertex[i])):
-		if vertex[i][j] == 1:
-			G.add_edge(nodes[i],nodes[j])
+for i in range(0,len(grafos.vertex)):
+	for j in range(0,len(grafos.vertex[i])):
+		if grafos.vertex[i][j] == 1:
+			G.add_edge(grafos.nodes[i],grafos.nodes[j])
 
-for i in range(0,len(nodes)):
+for i in range(0,len(grafos.nodes)):
 	G.node[listaNodes[i]]['color'] = mapa.getColor(i)
-	nx.draw(G,pos,node_size = 1000,nodelist = nodes[i],node_color = color[nodes[i]])
+	nx.draw(G,pos,node_size = 1000,nodelist = grafos.nodes[i],node_color = color[mapa.getColor(i)])
 
 
 plt.axis('off')
