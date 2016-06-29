42d0723f6eb67cdf7ba49297b179c96b9519aa4b
paulo <paulo.cruz9@gmail.com>
2016-06-29 01:23:31 -0300

    implementação basica do arquivo teste.py com import interativo do dataset

--- teste.py
+++ teste.py
@@ -1,1 +1,18 @@
-from backtrack
+import sys
+from backtrack import Grafo
+
+if len(sys.argv) < 2:
+    exit("ta faltando coisa")
+
+filename = sys.argv[1]
+
+grafos = __import__("grafos."+filename, globals(), locals(), [filename, filename], 0)
+
+mapa = Grafo()
+mapa.setNodes(grafos.nodes)
+mapa.setVertex(grafos.vertex)
+
+if mapa.colora_node(0):
+    print("tudo ok")
+else:
+    print("não deu solução")
